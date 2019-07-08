e917c0db365a539ecfb96ac958350ecc1c565e20
rahma <rahmawati.putrianasari@asani.co.id>
2019-07-08 18:42:42 +0700

    init

---
+++ app/ModelTodo.php
@@ -0,0 +1,9 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class ModelTodo extends Model
+{
+   protected 
+   $table = 'todo'; //nama table yang kita buat lewat migration adalah todo
+}
---
+++ app/User.php
@@ -0,0 +1,8 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class User extends Model
+{
+   protected 
+$table = 'todo'; //nama table yang kita buat lewat migration adalah todo
+}
